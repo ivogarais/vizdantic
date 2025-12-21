2a5157c3792955ef0ae4644aa426d5e6c9712ea1
Ivo Garais <ivogarais@titanhq.com>
2025-12-21 22:45:01 +0000

    initial viz.py and misc
    the initial data class structure for two types of charting

---
+++ vizdanic/__init__.py
@@ -0,0 +1,3 @@
+from .viz import ChartSpec, XYSpec, PointsSpec
+
+__all__ = ["ChartSpec", "XYSpec", "PointsSpec"]
---
+++ vizdanic/viz.py
@@ -0,0 +1,23 @@
+from enum import Enum
+from typing import Optional, Literal
+from pydantic import BaseModel, Field
+
+
+class ChartSpec(BaseModel):
+    chart: str
+    title: Optional[str] = None
+    legend_title: Optional[str] = None
+
+
+class XYSpec(ChartSpec):
+    kind: Literal["xy"] = Field("xy")
+    x: str
+    y: str
+    series: Optional[str] = None
+
+
+class PointSpec(ChartSpec):
+    kind: Literal["points"] = Field("points")
+    x: str
+    y: str
+    series: Optional[str] = None
