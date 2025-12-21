9ee17fe0b73f9d06642a72b7c5c84db5b26c6383
Ivo Garais <ivogarais@titanhq.com>
2025-12-21 22:54:15 +0000

    more complete enums

---
+++ vizdanic/enums.py
@@ -0,0 +1,40 @@
+# vizdantic/enums.py
+from enum import Enum
+
+
+class XYChart(str, Enum):
+    bar = "bar"
+    line = "line"
+    area = "area"
+
+
+class PointsChart(str, Enum):
+    scatter = "scatter"
+
+
+class DistributionChart(str, Enum):
+    histogram = "histogram"
+    box = "box"
+    violin = "violin"
+
+
+class PartsChart(str, Enum):
+    pie = "pie"
+
+
+class MatrixChart(str, Enum):
+    heatmap = "heatmap"
+
+
+class FlowChart(str, Enum):
+    sankey = "sankey"
+
+
+class HierarchyChart(str, Enum):
+    treemap = "treemap"
+    sunburst = "sunburst"
+
+
+class GeoChart(str, Enum):
+    choropleth = "choropleth"
+    scatter = "scatter"
